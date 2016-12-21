a82875cf67ee9f68d2ce631bb9632c52d5a17bf0
biciclista <biciclista@gmail.com>
2016-12-21 13:33:52 +0100

    Initial commit.

---
+++ ClubServer/src/main/java/com/bcclst/club/server/ClubServerApplication.java
@@ -0,0 +1,12 @@
+package com.bcclst.club.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubServerApplication.class, args);
+	}
+}
---
+++ ClubServer/src/test/java/com/bcclst/club/server/dto/ClubDtoTest.java
@@ -0,0 +1,48 @@
+package com.bcclst.club.server.dto;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.bcclst.club.server.ClubDto;
+import com.bcclst.common.util.StringUtil;
+
+public class ClubDtoTest {
+	private final Long ID_OK = 0L;
+	private final String NAME_OK = "Club Deportivo";
+	private final String SHORTNAME_OK = "CLUB";
+
+	private static Validator validator;
+
+	@BeforeClass
+	public static void setUp() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+
+	@Test
+	public void ifParametersOkPasses() {
+		ClubDto club = new ClubDto(ID_OK, NAME_OK, SHORTNAME_OK);
+		
+		Set<ConstraintViolation<ClubDto>> violations = validator.validate(club);
+		assertEquals(0, violations.size());
+	}
+
+	@Test
+	public void ifShortStringsValidationFails() {
+		ClubDto club = new ClubDto(ID_OK, NAME_OK, StringUtil.createStringWithLength(2));
+
+		Set<ConstraintViolation<ClubDto>> violations = validator.validate(club);
+
+		assertEquals(1, violations.size());
+		assertEquals("size must be between 3 and 5", violations.iterator().next().getMessage());
+	}
+}
